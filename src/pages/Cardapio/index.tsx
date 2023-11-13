2f74dca1cfdb374fdbecde09a6413286fb681205
SecreTI <secreti@Mini-de-SecreTI.lan>
2023-11-13 14:33:51 -0300

    Importe o logo.svg da forma correta e teste do comando npm run build

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,10 +1,11 @@
+import { ReactComponent as Logo} from '../../assets/logo.svg'
 import styles from './Cardapio.module.scss'
 
 export default function Cardapio() {
   return (
     <main>
-        <nav className="menu">
-
+        <nav className={styles.menu}>
+            <Logo />
         </nav>
     </main>
   )
